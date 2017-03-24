f1f78529f1c9f11518e46372b9cd13b2c59ce293
Wangbarros <wangbarros@gmail.com>
2017-03-24 16:30:46 -0300

    Added some comments to function. Still not sure where to put it.

--- bradata/connection.py
+++ bradata/connection.py
@@ -68,16 +68,22 @@ def get_links(xml):
 
 def get_infraero(year="2015"):
 
+    """
+    Get all statistics xls files from Infraero website for a given year
+    :param year: string year, from 2017 to 2012
+    :return: links: A list of all the links downloaded
+    """
 
     database_links = set()
     conn = Connection()
     statistics_page = conn.perform_request("http://www.infraero.gov.br/index.php/br/estatisticas/estatisticas.html")
     links = get_links(statistics_page["content"])
+
     for link in links:
         if (('Estatistica' in link['href']) and (year in link['href'])):
-            link2 = str(link)
-            link2 = link2.split('"')[1]
-            complete_link = "http://www.infraero.gov.br" + str(link2)
+            file_name = str(link)
+            file_name = file_name.split('"')[1]
+            complete_link = "http://www.infraero.gov.br" + str(file_name)
             database_links.add(complete_link)
 
     for link in database_links:
@@ -92,5 +98,5 @@ def get_infraero(year="2015"):
 
     return database_links
 
-aa = get_infraero()
+links_downloaded = get_infraero()
 
