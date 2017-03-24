67482cf5e916747ac7712afbc0e6310794937efe
Wangbarros <wangbarros@gmail.com>
2017-03-24 16:24:13 -0300

    Added function for Infraero data download. Not sure where to put it tough.

--- bradata/connection.py
+++ bradata/connection.py
@@ -1,5 +1,7 @@
 import requests
 import time
+from bs4 import BeautifulSoup
+import requests
 
 class Connection():
     """
@@ -52,4 +54,43 @@ class Connection():
                 time.sleep(5)
                 print('Trying Again...')
                 continue
+
+def get_links(xml):
+    """
+    Get all links from a html page
+    :param xml: string of a html page
+    :return: links: A list of all the link in the page
+    """
+    soup = BeautifulSoup(xml, 'lxml')
+    links = soup("a")
+
+    return links
+
+def get_infraero(year="2015"):
+
+
+    database_links = set()
+    conn = Connection()
+    statistics_page = conn.perform_request("http://www.infraero.gov.br/index.php/br/estatisticas/estatisticas.html")
+    links = get_links(statistics_page["content"])
+    for link in links:
+        if (('Estatistica' in link['href']) and (year in link['href'])):
+            link2 = str(link)
+            link2 = link2.split('"')[1]
+            complete_link = "http://www.infraero.gov.br" + str(link2)
+            database_links.add(complete_link)
+
+    for link in database_links:
+        name = str.split(link,"/")[-1]
+        print("Downloading: ", link)
+        resp = requests.get(link)
+        output = open(year + "_" + str(name), 'wb')
+        output.write(resp.content)
+        output.close()
+
+        time.sleep(0.05)
+
+    return database_links
+
+aa = get_infraero()
 
