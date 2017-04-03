8dcd6f2a0885372065be7cbbd31a749cc8566f1f
odanoburu <odanoburu@users.noreply.github.com>
2017-04-02 21:43:01 -0300

    update get() to handle bad zip files

--- bradata/cgu/ceis.py
+++ bradata/cgu/ceis.py
@@ -20,14 +20,18 @@ def get(date=None):
     params = {'a': date.year, 'm': '{:02d}'.format(date.month), 'd': '{:02d}'.format(date.day), 'consulta': 'CEIS'}
     r = requests.get('http://arquivos.portaldatransparencia.gov.br/downloads.asp', stream=True, params=params, timeout=1)
     if r.status_code == 200:
-        z = zipfile.ZipFile(io.BytesIO(r.content))
-        filename = z.namelist()[0]
-        with z.open(filename) as f:
-            latinceis = f.read()
-        ceis = latinceis.decode('cp1252').replace('\x00', '')  # http://www.portaldatransparencia.gov.br/faleConosco/perguntas-tema-download-dados.asp
-        with open(os.path.join(bradata.__download_dir__, filename), mode='wt', encoding='utf8') as f:
-            f.write(ceis)
-        return "CEIS downloaded to {}".format(bradata.__download_dir__)
+        request_content = io.BytesIO(r.content)
+        if zipfile.is_zipfile(request_content):
+            z = zipfile.ZipFile(request_content)
+            filename = z.namelist()[0]
+            with z.open(filename) as f:
+                latinceis = f.read()
+            ceis = latinceis.decode('cp1252').replace('\x00', '')  # http://www.portaldatransparencia.gov.br/faleConosco/perguntas-tema-download-dados.asp
+            with open(os.path.join(bradata.__download_dir__, filename), mode='wt', encoding='utf8') as f:
+                f.write(ceis)
+            return "CEIS downloaded to {}".format(bradata.__download_dir__)
+        else:
+            return "file from this date is not available. website gave\"\"\" {}\"\"\" as a reply. please try a later date.".format(r.text)  # stopped here
     else:
         print("the request failed. the portal da transparência server may be down, or it may have changed its address. please report the latter to the mantainers.")
         r.raise_for_status()
