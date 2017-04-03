0fb13da816897278c2d5886dcaa99dc8edd287dc
odanoburu <bcclaro@protonmail.ch>
2017-04-03 11:57:30 -0300

    add warning for package-wise use

--- bradata/connection.py
+++ bradata/connection.py
@@ -1,7 +1,10 @@
-import tqdm
+#import tqdm
 import time
 import requests
 
+# how to use the warning below: do something equivalent to `from bradata.connection import stale_url_warning` and then
+# `stale_url_warning.format((req.status_code, req.text, website.name, website.url)
+stale_url_warning = """the request failed with code {}:\n"{}"\nthe {} server may be down, or it may have changed its address or architecture.\nplease report the latter to the maintainers.\nyou can check if the website is online at {}"""
 
 class Connection:
     """
@@ -11,7 +14,6 @@ class Connection:
     def perform_request(self, url, nr_tries=5, binary=False):
         """
         Perform a request handling exception and server errors printing status
-
         :param url: string
         :param nr_tries: int
         :return: dict :: status : ok/error, content: xml/url, [error_type, error_desc] if error
@@ -56,4 +58,3 @@ class Connection:
                 time.sleep(5)
                 print('Trying Again...')
                 continue
-
